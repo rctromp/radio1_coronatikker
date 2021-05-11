3ebd4a4f26cf326b87312b063f9755f5c764319d
Reinier <rctromp@protonmail.com>
2021-05-11 15:35:29 +0200

    week 11 mei tikker

--- execute.R
+++ execute.R
@@ -2,6 +2,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # setwd("C:/Users/r.tromp/Code/radio1_coronatikker")
 source("daily_tikker_update.R")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("plotjes_scripts/cases_graph.R")
 source("plotjes_scripts/IC_graphs.R")
 
